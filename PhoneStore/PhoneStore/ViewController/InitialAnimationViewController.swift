f08b784a9974f3a23400bc06080870f8d9932031
Agustin Errecalde <agustine005@hotmail.com>
2021-03-19 23:52:58 -0300

    WIP: check if db copy was saved the first day of month

--- PhoneStore/PhoneStore/ViewController/InitialAnimationViewController.swift
+++ PhoneStore/PhoneStore/ViewController/InitialAnimationViewController.swift
@@ -8,15 +8,18 @@
 import UIKit
 import Lottie
 
-class InitialAnimationViewController: UIViewController {
+final class InitialAnimationViewController: UIViewController {
     
     private let successAnimationView = AnimationView(name: "loaderApple")
     @IBOutlet weak var aniView: UIView!
+    private var needSaveCopy = false
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.layer.insertSublayer(createCustomGradiend(view: view), at: 0)
         configureAnimation(animationView: successAnimationView)
+        checkIfNeedSaveCopy()
     }
     
     private func configureAnimation(animationView: AnimationView) {
@@ -35,9 +38,37 @@ class InitialAnimationViewController: UIViewController {
             } completion: { (finish) in
                 animationView.removeFromSuperview()
                 //MARK: - ADD SECURITY COPY OF DB
-                self.goToLogin()
+                self.checkIfNeedSaveCopy()
             }
         }
+    }
+    
+    private func checkIfNeedSaveCopy() {
+        let today = Date()
+        let wasSavedData = defaults.bool(forKey: defaultsKeys.needSaveKey)
+        
+        if today == today.startOfMonth() && wasSavedData {
+            presentAlertControllerWithDoubleAction(title: "Ops!", message: "Desea guardar una copia de seguridad?", delegate: self) { (actionOk) in
+                self.saveCopy()
+            } completionFailed: { (actionCancel) in
+                self.dontSaveCopy()
+            }
+        } else {
+            goToLogin()
+        }
+    }
+    
+    private func saveCopy() {
+        //MARK:- Save success
+        defaults.set(true, forKey: defaultsKeys.needSaveKey)
+        //MARK:- Save failed
+        defaults.set(false, forKey: defaultsKeys.needSaveKey)
+        
+        goToLogin()
+    }
+    
+    private func dontSaveCopy() {
+        defaults.set(false, forKey: defaultsKeys.needSaveKey)
     }
     
     private func goToLogin() {
@@ -46,4 +77,13 @@ class InitialAnimationViewController: UIViewController {
         newViewController.modalPresentationStyle = .fullScreen
         self.present(newViewController, animated: true, completion: nil)
     }
+}
+
+extension Date {
+    func startOfMonth() -> Date {
+        return Calendar.current.date(from: Calendar.current.dateComponents([.year, .month], from: Calendar.current.startOfDay(for: self)))!
+    }
+}
+struct defaultsKeys {
+    static let needSaveKey = "firstStringKey"
 }
